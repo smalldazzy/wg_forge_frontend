fd27d11ce316778c099ff6f5ce27982f41252198
smalldazzy <oleg.nod@gmail.com>
2019-03-08 01:39:17 +0300

    rebuilt using dom, task 3(80%)

--- src/app.js
+++ src/app.js
@@ -4,85 +4,139 @@ import users from '../data/users.json';
 import companies from '../data/companies.json';
 
 export default (function () {
+    var table=document.createElement('table');
+    var thead=document.createElement('thead');
+    table.appendChild(thead);
+    var head =['transaction_id','user_id','created_at','total','card_number','card_type','order_country','order_ip'];
+    thead.innerHTML='<th>Transaction ID</th><th>User Info</th><th>Order Date</th><th>Order Amount</th><th>Card Number</th><th>Card Type</th><th>Location</th>';
+    var tbody=document.createElement('tbody');
+    table.appendChild(tbody);
+    document.getElementById('app').appendChild(table);
+    for (var i=0;i<orders.length;i++)
+    {
+      var newRow=tbody.insertRow(i);
+      newRow.id=`order_${i+1}`;
 
-    function jsontable(objec){
-      //var cols = Object.keys(objec[0]);
-      var cols=["transaction_id","user_id","created_at","total",
-      "card_number","card_type", "order_country"]
-    //  console.log(cols);
-      var headerRow = '';
-      var bodyRows = '';
+        for (var k=0;k<7;k++)
+        {
+          var td=document.createElement('td');
+          switch(k){
+            //case userdiv
+            case 1:
+              for (var n=0;n<users.length;n++){
+                if (users[n].id==orders[i][head[k]]) {
+                  if (users[n].gender=="Male") {
+                    td.innerHTML=`<a href="#">Mr. ${users[n].first_name} ${users[n].last_name}</a>`;
+                  } else if (users[n].gender=="Female") {
+                    td.innerHTML=`<a href="#">Mrs. ${users[n].first_name} ${users[n].last_name}</a>`;
+                  } else {td.innerHTML='something went wrong';}
+                }
+               /*for_n otsuda*/
+              td.className='user_data';
+              var div=document.createElement('div');
+              div.className='user-details';
+              for (var m=0;m<4;m++){
+                var p=document.createElement('p');
+                switch (m) {
+                  case 0:
+                    //console.log(users[n].first_name+users[n].last_name);
+                    p.innerHTML=`Birthday ${convert_d(users[n].birthday)}`;
+                    break;
+                  case 1:
+                    p.innerHTML='<img src="" width="100px">';
+                    break;
+                  case 2:
+                    for (var h=0;h<companies.length;h++){
+                      if (users[n].company_id==companies[h].id){
+                        //console.log(users[n].company_id);
+                        //4218
+                       p.innerHTML=`Company: ${companies[h].id}`;
+                        //10
+                      }
+                    }
 
-      var  classes = '';
+                    break;
+                  case 3:
+                    p.innerHTML=`Industry: `;
+                    break;
+                  default:
 
-      function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-      }
+                }
+                div.appendChild(p);/*for_n----->*/}
 
-      cols.map(function(col) {
-        headerRow += '<th>' + capitalizeFirstLetter(col) + '</th>';
-      });
-      headerRow='<th>Transaction ID</th><th>User Info</th><th>Order Date</th><th>Order Amount</th><th>Card Number</th><th>Card Type</th><th>Location</th>'
-      var id_count=1;
-      objec.map(function(row) {
-        bodyRows += `<tr id=order_${id_count}>`;
-        id_count++;
-
-        cols.map(function(colName) {
-          var t='';
-        //  if (colName=="created_at") {return convert(row[colName]);}
-          if (colName=="order_country"){
-            t=row[colName]+' ('+row["order_ip"]+')';
-            bodyRows += '<td>' + t + '</td>';
-          } else if (colName=="created_at") {
-            bodyRows += '<td>' + convert(row[colName]) + '</td>';
-          }
-          else if (colName=="card_number") {
-              bodyRows += '<td>' + row[colName].substr(0,2) +'xxxxxxxxxx'+row[colName].substr(-4)+ '</td>';
-            }
-          else if (colName=="user_id") {
-              //bodyRows += '<td class="user_data">' + row[colName] + '</td>';}
-              for (var i=0;i<users.length;i++){
-                if (users[i].id==row[colName]) {
-                  if (users[i].gender=="Male") {
-                      bodyRows += '<td class="user_data">' + '<a href="#">Mr. '+ users[i].first_name +' '+ users[i].last_name + '</a></td>';
-                  } else if (users[i].gender=="Female") {
-                    bodyRows += '<td class="user_data">' + '<a href="#">Mrs. '+ users[i].first_name +' '+ users[i].last_name +'</a></td>';
-                  } else {bodyRows += '<td class="user_data">' +'Oooops'+ row[colName] + '</td>';}
-              }}}
-          else if (colName=="total") {
-                bodyRows += '<td>' + '$'+row[colName] + '</td>';
               }
-          else {
-            bodyRows += '<td>' + row[colName] + '</td>';
+              td.appendChild(div);
+              break;
+            //case datetime
+            case 2:
+              td.innerHTML=convert(orders[i][head[k]]);
+              break;
+            //case total
+            case 3:
+              td.innerHTML=`$${orders[i][head[k]]}`;
+              break;
+            //case card_number
+            case 4:
+              td.innerHTML=`${orders[i][head[k]].substr(0,2)}xxxxxxxxxx${orders[i][head[k]].substr(-4)}`;
+              break;
+            //case Location
+            case 6:
+              td.innerHTML=`${orders[i][head[k]]} (${orders[i][head[k+1]]})`;
+              break;
+            default:
+            td.innerHTML=orders[i][head[k]];
           }
+          newRow.appendChild(td);
 
-        //  bodyRows += '<td>' + t + '</td>';
-      })
+        //  col.innerHTML=orders[i].[k];
 
-        bodyRows += '</tr>';
-      });
-    //  console.log(bodyRows);
-      return '<table><thead><tr>' +
-         headerRow +
-         '</tr></thead><tbody>' +
-         bodyRows +
-         '</tbody></table>';
+        }
+
 
     }
-    function convert(timestamp){
-    var a = new Date(timestamp * 1000);
-    var year = a.getFullYear();
-    var month = a.getMonth()+1;
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-    var time = date + '/' + month + '/' + year + ' ' + hour + ':' + min + ':' + sec ;
-    return time;
-  }
 
-      document.getElementById("app").innerHTML=jsontable(orders);
+
+
+    function convert(timestamp){
+      var a = new Date(timestamp * 1000);
+      var year = a.getFullYear();
+      var month = a.getMonth()+1;
+      var date = a.getDate();
+      var hour = a.getHours();
+      var min = a.getMinutes();
+      var sec = a.getSeconds();
+      var time = date + '/' + month + '/' + year + ' ' + hour + ':' + min + ':' + sec ;
+      return time;
+    }
+    function convert_d(timestamp){
+      var a = new Date(timestamp * 1000);
+      var year = a.getFullYear();
+      var month = a.getMonth()+1;
+      var date = a.getDate();
+      var time = date + '/' + month + '/' + year ;
+      return time;
+    }
+
+    console.log(orders[1][head[5]]);
+      //document.getElementById("app").innerHTML=jsontable(orders);
+      // var table=document.querySelector('table');
+      // table.onclick = function(event) {
+      //   var target = event.target;
+      //
+      //   // цикл двигается вверх от target к родителям до table
+      //   while (target != table) {
+      //     if (target.tagName == 'TD') {
+      //       // нашли элемент, который нас интересует!
+      //       highlight(target);
+      //       return;
+      //     }
+      //     target = target.parentNode;
+      //   }
+      //   // возможна ситуация, когда клик был вне <td>
+      //   // если цикл дошёл до table и ничего не нашёл,
+      //   // то обработчик просто заканчивает работу
+      // }
+
     // YOUR CODE GOES HERE
     // next line is for example only
     document.getElementById("app").innerHTML += "<h1>Hello WG Forge</h1>";
