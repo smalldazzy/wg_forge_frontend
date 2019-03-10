8a64ae0e5ebd4302528a34e75807cb5dd0486a97
smalldazzy <oleg.nod@gmail.com>
2019-03-10 17:13:51 +0300

    task 5

--- src/app.js
+++ src/app.js
@@ -11,12 +11,12 @@ export default (function () {
     thead.innerHTML='<th style="cursor:pointer">Transaction ID</th><th style="cursor:pointer">User Info</th><th style="cursor:pointer">Order Date</th><th style="cursor:pointer">Order Amount</th><th>Card Number</th><th style="cursor:pointer">Card Type</th><th style="cursor:pointer">Location</th>';
     var tbody=document.createElement('tbody');
     table.appendChild(tbody);
-
+    let count=0;
     for (var i=0;i<orders.length;i++)
     {
       var newRow=tbody.insertRow(i);
       newRow.id=`order_${i+1}`;
-
+      count=i;
         for (var k=0;k<7;k++)
         {
           var td=document.createElement('td');
@@ -146,11 +146,6 @@ export default (function () {
           console.log(target.cellIndex);
           sortGrid(target.cellIndex);
         }
-        if (target.tagName == 'SPAN') {
-          // Если TH -- сортируем
-          console.log('spalili');
-          console.log(target);
-        }
         target = target.parentNode;
       }
       //console.log('missclick');
@@ -231,12 +226,125 @@ export default (function () {
         table.appendChild(tbody);
 
       }
+      function findMedian(data) {
+        // extract the .values field and sort the resulting array
+        var m = data.map(function(v) {
+          return v.values;
+        }).sort(function(a, b) {
+          return a - b;
+        });
+        var middle = Math.floor((m.length - 1) / 2); // NB: operator precedence
+        if (m.length % 2) {
+          return m[middle];
+        } else {
+          return (m[middle] + m[middle + 1]) / 2.0;
+        }
+      }
+      function stat() {
+        for (let i=0;i<5;i++){
+          switch (i){
+            case 0:{
+              let rowStats=tbody.insertRow(count+1);
+              let td=document.createElement('td');
+              td.innerHTML='Orders Count';
+              rowStats.appendChild(td);
+              let td1=document.createElement('td');
+              td1.innerHTML=count+1;
+              rowStats.appendChild(td1);
+              }
+              break;
+            case 1:{
+              let rowStats=tbody.insertRow(count+2);
+              let td=document.createElement('td');
+              td.innerHTML='Orders Total';
+              rowStats.appendChild(td);
+              //td.innerHTML=count+2;
+              let td1=document.createElement('td');
+              let total=0;
+              for (let i=0;i<orders.length;i++){
+                total+=Number(orders[i]['total']);
+              }
+              td1.innerHTML='$ '+total.toFixed(2);
+              rowStats.appendChild(td1);
+              }
+              break;
+            case 2:{
+              let rowStats=tbody.insertRow(count+3);
+              let td=document.createElement('td');
+              td.innerHTML='Median Value';
+              rowStats.appendChild(td);
+              //td.innerHTML=count+2;
+              let td1=document.createElement('td');
+              let total=[];
+              for (let i=0;i<orders.length;i++){
+                total[i]=Number(orders[i]['total']).toFixed(2);
+              }
+              total.sort(function(a, b) { return a - b;});
+              // console.log(total[Math.floor((total.length - 1) / 2)]);
+              // console.log(total);
+              td1.innerHTML='$ '+total[Math.floor((total.length - 1) / 2)];
+              rowStats.appendChild(td1);
+              break;
+            }
+            case 3:{
+              let rowStats=tbody.insertRow(count+4);
+              let td=document.createElement('td');
+              td.innerHTML='Average Check';
+              rowStats.appendChild(td);
+              //td.innerHTML=count+2;
+              let td1=document.createElement('td');
+              let total=0;
+              for (let i=0;i<orders.length;i++){
+                total+=Number(orders[i]['total']);
+              }
+              td1.innerHTML='$ '+(total.toFixed(2)/(count+1)).toFixed(2);
+              rowStats.appendChild(td1);
+              }
+              break;
+            case 4:{
+              let rowStats1=tbody.insertRow(count+5);
+              let rowStats2=tbody.insertRow(count+6);
+              let td1=document.createElement('td');
+              td1.innerHTML='Average Check (Female)';
+              rowStats1.appendChild(td1);
+              //td.innerHTML=count+2;
+              let td2=document.createElement('td');
+              let total_fem=0, total_m=0,count_fem=0,count_m=0;
+              for (let i=0;i<orders.length;i++){
+                for(let n=0;n<users.length;n++){
+                  if (orders[i]['user_id']==users[n].id){
+                    if (users[n].gender=='Male') {
+                      count_m++;
+                      total_m+=Number(orders[i]['total']);
+                    }
+                    if (users[n].gender=='Female') {
+                      count_fem++;
+                      total_fem+=Number(orders[i]['total']);
+                    }
+                  }
+                }
+              }
+              console.log(total_fem, count_fem);
+              console.log(total_m, count_m);
+              let td3=document.createElement('td');
+              td3.innerHTML='Average Check (Male)';
+              rowStats2.appendChild(td3);
+              td2.innerHTML='$ '+(total_fem/count_fem).toFixed(2);
+              let td4=document.createElement('td');
+              td4.innerHTML='$ '+(total_m/count_m).toFixed(2);
+              rowStats1.appendChild(td2);
+              rowStats2.appendChild(td4);
+              }
+              break;
+          }
+        }
+      }
+      stat();
     // table.onclick = function(event) {
     // if (!event.target.tagName('a')) return;
     // console.log('jopa');
     // event.target.nextSibling.hidden = !event.target.nextSibling.hidden;
     // }
-
     document.getElementById('app').appendChild(table);
     // YOUR CODE GOES HERE
     // next line is for example only
